5dbd872f3afd237654000e1546d29fc214409511
Keyeric <keyeric.bristol@gmail.com>
2020-03-18 09:54:48 -0700

    i feel like im close but idk

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { buyItem } from "../actions/carActions";
+import { connect } from "react-redux";
 
 const AdditionalFeature = props => {
+  console.log("this is the add button", props);
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => buyItem(props)}>
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
-
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  return {
+    addtionalPrice: state.additionalPrice,
+    features: state.car.features,
+    additionalFeatures: state.additionalFeatures
+  };
+};
+export default connect(mapStateToProps, {
+  buyItem
+})(AdditionalFeature);
